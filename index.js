a8ad2f9c2934a0831a1e106a92036b5506cea87e
Mercy Maruga <mercy.maruga@student.moringaschool.com>
2022-08-19 23:50:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution here
+function findMatching(Array, string){
+    const a = Array.filter(function (name){
+          
+            if( name.toUpperCase() === string.toUpperCase()){
+            return ([name])}
+          
+        }
+    )
+     return a;
+    }
+
+function fuzzyMatch(Array, string){
+    const x = 
+    Array.filter(function (name){
+          const length = string.length
+            if( name.slice(0, length) === string){
+            return [name]}
+          
+        }
+    )
+     return x;
+    }
+
+    function matchName(Array, string){
+        const driver = Array.filter(function (name){
+              
+            if( name['name'] === string){
+                return [name]}  
+              
+            }
+        )
+         return driver;
+        }
